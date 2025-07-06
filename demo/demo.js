7284c253e4d1000f5e32a8445c13656a6f93fdd6
basim-12 <f223684@cfd.nu.edu.pk>
2025-07-06 06:07:52 +0500

    proper communication, updated versions of compnents + additional bug fixes

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,6 @@
 const range_slider_integer = require('..')
 
-const opts = { min:1, max:10 }
+const opts = { min: 0, max: 10 }
 const rsi = range_slider_integer(opts)
 
 document.body.append(rsi)
--- src/index.js
+++ src/index.js
@@ -4,23 +4,54 @@ const input = require('@basim.asim123/input.integer')
 module.exports = range_slider_integer
 
 function range_slider_integer (opts) {
-    const el = document.createElement('div')
-    const shadow = el.attachShadow({ mode: 'closed' })
-    
-    const range_slider = range(opts, listen)
-    const input_integer = input(opts, listen)
+  const state = {
 
-    output = document.createElement('div')
-    output.innerText = 0
+  }
+  const el = document.createElement('div')
+  const shadow = el.attachShadow({ mode: 'closed' })
 
-    shadow.append(range_slider, input_integer, output)
+  const range_slider = range(opts, protocol)
+  const input_integer = input(opts, protocol)
 
-    return el
+  const rsi = document.createElement('div')
+  rsi.classList.add('rsi')
 
-    function listen (message) {
-        const { type, body } = message 
-        if (type == 'update') output.innerText = body 
-        console.log(message)
+  const style = document.createElement('style')
+  style.textContent = get_theme()
 
+  rsi.append(range_slider, input_integer)
+  shadow.append(rsi, style)
+
+  return el
+
+  function protocol (message, notify) {
+    const { from } = message
+    state[from] = { value: 0, notify }
+
+    return listen
+  }
+
+  function listen (message) {
+    const { from, type, body } = message
+    state[from].value = body
+
+    if (type == 'update') {
+      let notify
+      if (from == 'range-0') notify = state['input-integer-0'].notify
+      else if (from == 'input-integer-0') notify = state['range-0'].notify
+      notify({ type, body })
     }
+  }
+}
+
+function get_theme () {
+  return `
+    .rsi {
+      padding: 8%;
+      display: grid;
+      grid-template-columns: 8fr 1fr;
+      gap: 10px;
+      align-items: center;
+    }
+  `
 }
